5a1f2a4a8436ad0fbaa98f93a5fdd8f5deeff44b
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-28 15:04:27 +0100

    feat: main footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,30 @@
 export default function Footer(){
-    return (<h1>ciao</h1>)
+    return (
+        <footer>
+            <div className="container">
+                <div className="container-block">
+                    <div className="block">
+                        <img src="./img/buy-comics-digital-comics.png" alt="" />
+                        <a href="#">DIGITAL COMICS</a>
+                    </div>
+                    <div className="block">
+                        <img src="./img/buy-comics-merchandise.png" alt="" />
+                        <a href="#">DC MERCHANDISE</a>
+                    </div>
+                    <div className="block">
+                        <img src="./img/buy-comics-subscriptions.png" alt="" />
+                        <a href="#">SUBSCRIPTION</a>
+                    </div>
+                    <div className="block">
+                        <img src="./img/buy-comics-shop-locator.png" alt="" />
+                        <a href="#">COMIC SHOP LOCATOR</a>
+                    </div>
+                    <div className="block">
+                        <img src="./img/buy-dc-power-visa.svg" alt="" />
+                        <a href="#">DC POWER VISA</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
 }
