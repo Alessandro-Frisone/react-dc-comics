7332a4b32e5810646365701cc41c159c964bd448
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-28 14:29:21 +0100

    feat: header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,41 @@
+export default function Header(){
+    return (
+    <div className="container">
+        <img className="logo-img" src="./img/dc-logo.png" alt="logo"></img>
+        <nav className="nav-header">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+)
+}
