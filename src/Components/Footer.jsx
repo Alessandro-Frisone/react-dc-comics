de50a9532d9f925276d0b0d8cff4823c355e931e
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-28 15:46:52 +0100

    feat: main footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,30 +1,130 @@
-export default function Footer(){
-    return (
-        <footer>
-            <div className="container">
-                <div className="container-block">
-                    <div className="block">
-                        <img src="./img/buy-comics-digital-comics.png" alt="" />
-                        <a href="#">DIGITAL COMICS</a>
-                    </div>
-                    <div className="block">
-                        <img src="./img/buy-comics-merchandise.png" alt="" />
-                        <a href="#">DC MERCHANDISE</a>
-                    </div>
-                    <div className="block">
-                        <img src="./img/buy-comics-subscriptions.png" alt="" />
-                        <a href="#">SUBSCRIPTION</a>
-                    </div>
-                    <div className="block">
-                        <img src="./img/buy-comics-shop-locator.png" alt="" />
-                        <a href="#">COMIC SHOP LOCATOR</a>
-                    </div>
-                    <div className="block">
-                        <img src="./img/buy-dc-power-visa.svg" alt="" />
-                        <a href="#">DC POWER VISA</a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="container-block">
+          <div className="block">
+            <img src="./img/buy-comics-digital-comics.png" alt="" />
+            <a href="#">DIGITAL COMICS</a>
+          </div>
+          <div className="block">
+            <img src="./img/buy-comics-merchandise.png" alt="" />
+            <a href="#">DC MERCHANDISE</a>
+          </div>
+          <div className="block">
+            <img src="./img/buy-comics-subscriptions.png" alt="" />
+            <a href="#">SUBSCRIPTION</a>
+          </div>
+          <div className="block">
+            <img src="./img/buy-comics-shop-locator.png" alt="" />
+            <a href="#">COMIC SHOP LOCATOR</a>
+          </div>
+          <div className="block">
+            <img src="./img/buy-dc-power-visa.svg" alt="" />
+            <a href="#">DC POWER VISA</a>
+          </div>
+        </div>
+      </div>
+      <div className="container-main-footer">
+        <div className="container-footer">
+          <div className="first-ul">
+            <h3>DC COMICS</h3>
+            <ul>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+            <h3 className="second-title">SHOP</h3>
+            <ul>
+              <li>
+                <a href="#">Shop DC</a>
+              </li>
+              <li>
+                <a href="#">Shop DC Collectibles</a>
+              </li>
+            </ul>
+          </div>
+          <div className="second-ul">
+            <h3>DC</h3>
+            <ul>
+              <li>
+                <a href="#">Terms Of Use</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy (New)</a>
+              </li>
+              <li>
+                <a href="#">Ad Choices</a>
+              </li>
+              <li>
+                <a href="#">Advertising</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Subscriptions</a>
+              </li>
+              <li>
+                <a href="#">Talent Workshops</a>
+              </li>
+              <li>
+                <a href="#">CPSC Certificates</a>
+              </li>
+              <li>
+                <a href="#">Ratings</a>
+              </li>
+              <li>
+                <a href="#">Shop Help</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="third-ul">
+          <h3 className="third-title">SITES</h3>
+            <ul>
+              <li>
+                <a href="#">DC</a>
+              </li>
+              <li>
+                <a href="#">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="#">DC Kids</a>
+              </li>
+              <li>
+                <a href="#">DC Universe</a>
+              </li>
+              <li>
+                <a href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+          <div className="img-footer-container">
+            <img src="./img/dc-logo-bg.png" alt=""/>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
