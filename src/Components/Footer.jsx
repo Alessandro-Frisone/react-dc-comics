14ddf50e59c611488d3c1e9e78739d6ee19d8388
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-29 19:28:50 +0100

    feat: add .map for the main footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,5 @@
+import mainFooterLink from "../data/mainFooterLinks";
+
 export default function Footer() {
   return (
     <footer>
@@ -25,121 +27,44 @@ export default function Footer() {
           </div>
         </div>
       </div>
+
       <div className="container-main-footer">
         <div className="container-footer">
-          <div className="first-ul">
-            <h3>DC COMICS</h3>
-            <ul>
-              <li>
-                <a href="#">Characters</a>
-              </li>
-              <li>
-                <a href="#">Comics</a>
-              </li>
-              <li>
-                <a href="#">Movies</a>
-              </li>
-              <li>
-                <a href="#">TV</a>
-              </li>
-              <li>
-                <a href="#">Games</a>
-              </li>
-              <li>
-                <a href="#">Videos</a>
-              </li>
-              <li>
-                <a href="#">News</a>
-              </li>
-            </ul>
-            <h3 className="second-title">SHOP</h3>
-            <ul>
-              <li>
-                <a href="#">Shop DC</a>
-              </li>
-              <li>
-                <a href="#">Shop DC Collectibles</a>
-              </li>
-            </ul>
-          </div>
-          <div className="second-ul">
-            <h3>DC</h3>
-            <ul>
-              <li>
-                <a href="#">Terms Of Use</a>
-              </li>
-              <li>
-                <a href="#">Privacy Policy (New)</a>
-              </li>
-              <li>
-                <a href="#">Ad Choices</a>
-              </li>
-              <li>
-                <a href="#">Advertising</a>
-              </li>
-              <li>
-                <a href="#">Jobs</a>
-              </li>
-              <li>
-                <a href="#">Subscriptions</a>
-              </li>
-              <li>
-                <a href="#">Talent Workshops</a>
-              </li>
-              <li>
-                <a href="#">CPSC Certificates</a>
-              </li>
-              <li>
-                <a href="#">Ratings</a>
-              </li>
-              <li>
-                <a href="#">Shop Help</a>
-              </li>
-              <li>
-                <a href="#">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-          <div className="third-ul">
-          <h3 className="third-title">SITES</h3>
-            <ul>
-              <li>
-                <a href="#">DC</a>
-              </li>
-              <li>
-                <a href="#">MAD Magazine</a>
-              </li>
-              <li>
-                <a href="#">DC Kids</a>
-              </li>
-              <li>
-                <a href="#">DC Universe</a>
-              </li>
-              <li>
-                <a href="#">DC Power Visa</a>
-              </li>
-            </ul>
-          </div>
-          <div className="img-footer-container">
-            <img src="./img/dc-logo-bg.png" alt=""/>
-          </div>
+          {mainFooterLink.map((firstUl) => {
+            return (
+              <div key={firstUl.id} className="link-section">
+                <h3>{firstUl.title}</h3>
+                <ul>
+                  {firstUl.link.map((link) => {
+                    return (
+                      <li key={link.id}>
+                        <a href={link.url}>{link.text}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+          <img src="./img/dc-logo-bg.png" alt=""/>
         </div>
       </div>
+      
       <div className="last-footer">
         <div className="container-last-footer">
-            <div className="left-footer">
-                <button className="sign-up">SIGN-UP NOW!</button>
+          <div className="left-footer">
+            <button className="sign-up">SIGN-UP NOW!</button>
+          </div>
+          <div className="right-footer">
+            <h3>FOLLOW US</h3>
+            <div className="img-last-footer">
+              <img src="./img/footer-facebook.png" alt="" />
+              <img src="./img/footer-twitter.png" alt="" />
+              <img src="./img/footer-youtube.png" alt="" />
+              <img src="./img/footer-pinterest.png" alt="" />
+              <img src="./img/footer-periscope.png" alt="" />
             </div>
-            <div className="right-footer">
-                <h3>FOLLOW US</h3>
-                <div className="img-last-footer">
-                    <img src="./img/footer-facebook.png" alt="" />
-                    <img src="./img/footer-twitter.png" alt="" />
-                    <img src="./img/footer-youtube.png" alt="" />
-                    <img src="./img/footer-pinterest.png" alt="" />
-                    <img src="./img/footer-periscope.png" alt="" />
-                </div>
-            </div>
+          </div>
         </div>
       </div>
     </footer>
