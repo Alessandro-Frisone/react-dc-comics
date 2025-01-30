3c1edb6b040ea573d0e0a5037e96a0bc4141b8fd
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-30 16:34:06 +0100

    feat: add hero img

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,24 +2,29 @@ import navBarData from "../data/navBarData";
 
 export default function Header() {
   return (
-    <div className="container">
-      <div className="nav-container">
-        <img className="logo-img" src="./img/dc-logo.png" alt="logo"></img>
-        <nav className="nav-header">
-          <ul>
-            {navBarData.map((navBar) => {
-              return (
-                <li
-                  key={navBarData.id}
-                  className={navBar.current ? "fregnetto" : ""}
-                >
-                  <a href={navBar.url}>{navBar.text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+    <header>
+      <div className="container">
+        <div className="nav-container">
+          <img className="logo-img" src="./img/dc-logo.png" alt="logo"></img>
+          <nav className="nav-header">
+            <ul>
+              {navBarData.map((navBar) => {
+                return (
+                  <li
+                    key={navBarData.id}
+                    className={navBar.current ? "fregnetto" : ""}
+                  >
+                    <a href={navBar.url}>{navBar.text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
+      <div className="hero-container">
+        <div className="hero"></div>
+      </div>
+    </header>
   );
 }
