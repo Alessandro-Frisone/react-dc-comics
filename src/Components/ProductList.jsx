03fe1392cbd39b075d45f811dc558d97adb86253
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-30 15:33:30 +0100

    feat: add CROPS and ciclo MAP for the card

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,18 @@
 import ProductCard from "./ProductCard";
+import comics from "../data/comicsData";
 
 export default function ProductList() {
   return (
     <section className="products">
       <div className="container">
         <div className="row">
-          <div className="col">
-            <ProductCard />
-          </div>
+          {comics.map((elm) => {
+            return (
+              <div className="col" key={comics.id}>
+                <ProductCard image={elm.thumb} title={elm.series} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
