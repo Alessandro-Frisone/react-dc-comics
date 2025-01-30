23947246521f10dff354b6ebb8abb6547c254e03
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-30 18:11:12 +0100

    feat: add hover on nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,17 +3,14 @@ import navBarData from "../data/navBarData";
 export default function Header() {
   return (
     <header>
-      <div className="container">
+      <div className="container-main">
         <div className="nav-container">
           <img className="logo-img" src="./img/dc-logo.png" alt="logo"></img>
           <nav className="nav-header">
             <ul>
               {navBarData.map((navBar) => {
                 return (
-                  <li
-                    key={navBarData.id}
-                    className={navBar.current ? "fregnetto" : ""}
-                  >
+                  <li key={navBarData.id}>
                     <a href={navBar.url}>{navBar.text}</a>
                   </li>
                 );
@@ -22,7 +19,7 @@ export default function Header() {
           </nav>
         </div>
       </div>
-        <div className="hero"></div>
+      <div className="hero"></div>
     </header>
   );
 }
